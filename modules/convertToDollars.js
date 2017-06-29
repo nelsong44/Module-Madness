52af54102fbb1fb4bbc5253be42dee043c969d47
nelsong44 <nelsong44@gmail.com>
2017-06-28 19:43:02 -0500

    changed dollarConversion function from the original one

--- modules/convertToDollars.js
+++ modules/convertToDollars.js
@@ -1,7 +1,7 @@
 //function to convert an integer to a dollar value
-function dollarConversion(min, max) {
+function dollarConversion(ranNum) {
   // return '$' + ranNum.toLocaleString();
-  return Math.floor(Math.random() * (1 + max - min) + min);
+  return '$' + ranNum.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 }// end dollarConversion
 
 module.exports = dollarConversion;
